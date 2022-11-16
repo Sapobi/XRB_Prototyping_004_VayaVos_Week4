e77703da19e1ea2d7f0f2084b2dcda202cf30f80
Sapobi <sapobinet@gmail.com>
2022-11-15 20:32:33 -0400

    Added 2 more songs

--- Assets/Scripts/Menu/ScoreManager.cs
+++ Assets/Scripts/Menu/ScoreManager.cs
@@ -3,37 +3,57 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-	public static ScoreManager Instance;
-	public AudioSource hitSFX;
-	public AudioSource missSFX;
-	public TextMeshPro scoreText;
-	static int comboScore;
+	[SerializeField] private AudioSource hitSFX;
+	[SerializeField] private AudioSource missSFX;
+	[SerializeField] private TMP_Text comboText, maxComboText, hitRatioText;
+	
+	private static ScoreManager _instance;
+	private static int _comboScore, _hitScore, _maxCombo, _fullComboCount;
+	static bool _fullCombo = true;
 
 	void Start()
 	{
-		Instance = this;
+		_instance = this;
 		SongManager.StartGame.AddListener(ResetScore);
+		SongManager.EndGame.AddListener(SetEndScore);
 	}
 
 	private static void ResetScore()
 	{
-		comboScore = 0;
+		_hitScore = 0;
+		_comboScore = 0;
+		_maxCombo = 0;
+		_fullComboCount = 0;
+		_fullCombo = true;
+	}
+
+	private void SetEndScore()
+	{
+		maxComboText.text = _fullCombo ? "FULL COMBO" : $"MAX COMBO\n{_maxCombo}";
+		hitRatioText.text = $"{_hitScore}/{_fullComboCount}";
 	}
 
 	public static void Hit()
 	{
-		comboScore += 1;
-		Instance.hitSFX.Play();
+		_fullComboCount += 1;
+
+		_hitScore += 1;
+		_comboScore += 1;
+		if (_comboScore > _maxCombo) _maxCombo = _comboScore;
+		_instance.hitSFX.Play();
 	}
 
 	public static void Miss()
 	{
-		ResetScore();
-		Instance.missSFX.Play();
+		_fullComboCount += 1;
+		
+		_comboScore = 0;
+		if (_fullCombo) _fullCombo = !_fullCombo;
+		_instance.missSFX.Play();
 	}
 
 	private void Update()
 	{
-		scoreText.text = comboScore.ToString();
+		comboText.text = _comboScore.ToString();
 	}
 }
